1ecb07adeb9fa7d494274214aa6516d2bbcc1037
Ben Taitelbaum <ben@coshx.com>
2013-06-25 04:20:20 -0400

    vanilla rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JoosyTodos::Application.config.secret_token = '2b2ef02023459547939f4cc0921befc7a7d9ed6ca2a583eac2dfe3edc4f17fc31bb5669bd76542cc20d5eda4c6553c04e9845fce1581ee0007e791b4556d1743'
